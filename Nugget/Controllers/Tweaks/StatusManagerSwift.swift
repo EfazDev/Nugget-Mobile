d86367456bf1f2218e54c067ede71a1520fe20cf
EfazDev <me@efaz.dev>
2024-10-25 21:39:21 -0500

    n

--- Nugget/Controllers/Tweaks/StatusManagerSwift.swift
+++ Nugget/Controllers/Tweaks/StatusManagerSwift.swift
@@ -20,22 +20,13 @@ import SwiftUI
         try FileManager.default.removeItem(at: URL.tweaksDirectory.appendingPathComponent("statusBarOverrides"))
         return Data()
     }
-    
+
     @objc func getDeviceVersion() -> String {
         return UIDevice.current.systemVersion
     }
     
     @objc func getOverridesFileURL() -> URL {
-        let fm = FileManager.default
-        let overridesURL = URL.tweaksDirectory.appendingPathComponent("statusBarOverrides")
-        if !fm.fileExists(atPath: overridesURL.path) {
-            // write new file to the app's documents directory
-            do {
-                try Data().write(to: overridesURL)
-            } catch {
-                print(error.localizedDescription)
-            }
-        }
+        
         return overridesURL
     }
 }
